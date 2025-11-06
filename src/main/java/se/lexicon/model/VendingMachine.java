fa0dffc5fe44f8029aff071eb7e4a2d58700cec9
Ashbinder666 <ashxela@gmail.com>
2025-11-06 11:34:27 +0100

    Added Currency and VendingMachine

---
+++ src/main/java/se/lexicon/model/VendingMachine.java
@@ -0,0 +1,12 @@
+package se.lexicon.model;
+
+public interface VendingMachine {
+
+    void addCurrency(Currency amount);
+    int getBalance();
+    Product request(int id);
+    String getDescription(int id);
+    String[] getProducts();
+
+
+}
